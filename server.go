fcc12d81d5613e455d9af07a0936448d8b176342
yasuno0327 <5315076@ed.tus.ac.jp>
2018-12-24 15:11:08 +0900

    Add ListFeature method

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	pb "grpc-tutorial/routeguide"
+	"math"
 	"sync"
 
 	"github.com/golang/protobuf/proto"
@@ -28,4 +29,32 @@ func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb
 
 	// Featureが見つからない場合は空のFeatureを返す
 	return &pb.Feature{Location: point}, nil
+}
+
+func (s *routeGuideServer) ListFeature(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// 範囲内にあるかどうか
+func inRange(point *pb.Point, rect *pb.Rectangle) bool {
+	// left: 経度の最小値 right: 経度の最大値 top: 緯度の最大値 bottom: 緯度の最小値
+	left := math.Min(float64(rect.Lo.Longititude), float64(rect.Hi.Longititude))
+	right := math.Max(float64(rect.Lo.Longititude), float64(rect.Hi.Longititude))
+	top := math.Max(float64(rect.Lo.Lattitude), float64(rect.Hi.Lattitude))
+	bottom := math.Min(float64(rect.Lo.Lattitude), float64(rect.Hi.Lattitude))
+
+	if float64(point.Longititude) >= left &&
+		float64(point.Longititude) <= right &&
+		float64(point.Lattitude) <= top &&
+		float64(point.Lattitude) >= bottom {
+		return true
+	}
+	return false
 }
